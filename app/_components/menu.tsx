7bda08fa23447bf2a50740c1a39f894ef0fa6789
zFelipez <felipefesilva19@gmail.com>
2025-12-30 21:53:10 -0300

    fix: correct fetch url in route handler call

--- app/_components/menu.tsx
+++ app/_components/menu.tsx
@@ -1,10 +1,11 @@
 
 import { Navigation } from "./navigation";
 import { Repo } from "../_components/navigation";
+import { headers } from "next/headers";
 
 export async function Menu(){
-    
-  const data: Repo[] = await fetch('http://localhost:3000/api/github',{ cache:'no-store'}).then(response => response.json())
+  const host = (await headers()).get('host');
+  const data: Repo[] = await fetch(`http://${host}/api/github`,{ cache:'no-store'}).then(response => response.json())
      
   return(
 
