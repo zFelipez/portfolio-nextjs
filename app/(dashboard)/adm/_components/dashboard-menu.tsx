6961752a8ff8b2e91ca9c2438fbdaaaf389da1f3
zFelipez <felipefesilva19@gmail.com>
2026-01-03 14:52:48 -0300

    fix: correct menu links

--- app/(dashboard)/adm/_components/dashboard-menu.tsx
+++ app/(dashboard)/adm/_components/dashboard-menu.tsx
@@ -9,8 +9,8 @@ export function DashboardMenu() {
 
   return (
     <nav className="flex w-full gap-2 items-center justify-center p-5">
-       <DashboardLink pathname={pathname} url="/adm" >Projects Settings </DashboardLink>
-       <DashboardLink pathname={pathname} url="/adm/projects" > <Home/></DashboardLink>
+       <DashboardLink pathname={pathname} url="/adm/projects" >Projects Settings </DashboardLink>
+       <DashboardLink pathname={pathname} url="/adm" > <Home/></DashboardLink>
        <DashboardLink pathname={pathname} url="/adm/personal/info" > Personal Info </DashboardLink>
 
       
